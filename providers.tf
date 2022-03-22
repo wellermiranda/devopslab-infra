cd136d10941ea77f27314406fa8acb84c4e06202
Vagrant User <vagrant@workspace>
2022-03-21 21:40:50 -0300

    App Engine Provision

--- providers.tf
+++ providers.tf
@@ -9,7 +9,13 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  # credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "regal-groove-344900"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+provider "google-beta" {
   project = "regal-groove-344900"
   region  = "us-west1"
   zone    = "us-west1-b"
