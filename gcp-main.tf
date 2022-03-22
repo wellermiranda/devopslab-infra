4a6205c56479d54e85cd5dda827946b6c66f2a94
Weller Miranda <weller@loft.com.br>
2022-03-22 19:18:42 -0300

    teste

--- gcp-main.tf
+++ gcp-main.tf
@@ -5,7 +5,6 @@ resource "google_app_engine_application" "app" {
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
-
   location = "us-central1"
   repository_id = "labdevops"
   description = "Imagens Docker"
