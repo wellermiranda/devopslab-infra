eb2b06c14e9b8d00efae37f038f7fab801eaea0e
Weller Miranda <weller@loft.com.br>
2022-03-22 19:21:30 -0300

    deploy infra

--- gcp-main.tf
+++ gcp-main.tf
@@ -5,6 +5,7 @@ resource "google_app_engine_application" "app" {
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
+  
   location = "us-central1"
   repository_id = "labdevops"
   description = "Imagens Docker"
